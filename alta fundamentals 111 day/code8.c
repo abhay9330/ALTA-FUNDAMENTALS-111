b97bbc1b67fe3d3b9ab9640b9bf47cca7bbf1e55
Abhay Pandey <abhaypandey5973@gmail.com>
2026-09-26 01:00:09 +0530

    Add files via upload

--- alta fundamentals 111 day/code8.c
+++ alta fundamentals 111 day/code8.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-int main() {
-    int n;
-    scanf("%d", &n);
-
-    if (n % 3 == 0 && n % 5 == 0)
+int main(){
+    int z;
+    scanf("%d",&z);
+    if (z % 3 == 0 && z % 5 == 0)
         printf("Yes");
     else
         printf("No");
-
-    return 0;
 }
